d45e67442945f82468b1e9e00a702c7eda5aa90f
Adam <wangadam019@gmail.com>
2023-11-08 01:59:36 -0500

    added general comments on html file

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
+  // set variable to menu links
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
+  // open menu and hamburger menu
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
